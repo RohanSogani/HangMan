288ddb8800dc5cc63dc37ee59e4bdeeb6a2f5604
RohanSogani <ronsogani@gmail.com>
2019-10-08 23:45:54 -0700

    Word Files and Random Word checkin

--- hangman.py
+++ hangman.py
@@ -1,39 +1,57 @@
 import time
+import json
+import random
+
+with open("Words.json") as wordFile:
+    dataSource = json.load(wordFile)
 
 name = input("What is your name? ")
 
-print("Hello, " + name +" Time to play new Hangman!!")
+print("Hello, " + name +" time to play new Hangman!!")
 
 time.sleep(1);
 
 print("Start Guessing....")
 time.sleep(0.5)
 
-word = "Heaven"
+play = 1;
+while play == 1:
+    word = random.choice(dataSource["data"])
+    guesses = ""
+    turns = 7
+    while turns > 0:
+        failed = 0
+        for char in word:
+            if char in guesses:
+                print (char, end=" ")
+            else:
+                print ("_", end=" ")
+                failed += 1
+        if failed == 0:
+            print("You Won")
+            break
 
-guesses = ""
+        print("You have", turns, "turns", end=" ")
+        guess = input("Guess a character --> ")
 
-turns = 7
+        guesses += guess
 
-while turns > 0:
-    failed = 0
-    for char in word:
-        if char in guesses:
-            print (char, end=" ")
-        else:
-            print ("_", end=" ")
-            failed += 1
-    if failed == 0:
-        print("You Won")
-        break
-
-    guess = input(" Guess a character --> ")
-
-    guesses += guess
-
-    if(guess not in word):
-        turns -= 1
-        print("Wrong")
-        print("You have ", +turns, " more guesses")
-        if(turns == 0):
-            print("You loose :'(")
+        if(guess not in word):
+            turns -= 1
+            print("Wrong")
+            print("You have ", +turns, " more guesses")
+            if(turns == 0):
+                print("You loose :'(")
+                print("The word was", word)
+                playKey = ''
+                while 1:
+                    playKey = input("Do you still want to continue? [Y/N]");
+                    if playKey == 'Y' or playKey == 'y':
+                        play = 1
+                        break
+                    elif playKey == 'N' or playKey == 'n':
+                        play = 0
+                        print("Bye")
+                        break
+                    else:
+                        print("Invalid input")
